2fd69d78b4349a9dc1764515e2e3daf9d8a022d5
AAbhiJ <abhiasawale510@gmail>
2023-10-20 07:51:55 +0530

    corrected Space complexity, Added Version 2 solution

--- leetcode/412. Fizz Buzz/solution.js
+++ leetcode/412. Fizz Buzz/solution.js
@@ -4,6 +4,8 @@
  * Time Complexity : O(N) - As we are need to iterate for n times (N - n (Number))
  * Space Complexity : O(1) - As we are creating a answerArr of size N, however this do not contribute to the Space complexity
  */
+
+/** Version 1 */
 var fizzBuzz = function (n) {
     let answerArr = [];
     for (let i = 1; i <= n; i++) {
@@ -12,6 +14,21 @@ var fizzBuzz = function (n) {
         if(i % 3 === 0) res = "Fizz";
         if (i % 3 === 0 && i % 5 === 0) res = "FizzBuzz";
 
+        answerArr.push(res)
+
+    }
+    return answerArr;
+};
+
+/** Version 2 */
+var fizzBuzz = function (n) {
+    let answerArr = [];
+    for (let i = 1; i <= n; i++) {
+        let res = ``;
+
+        if(i % 3 === 0) res += "Fizz";
+        if(i % 5 === 0) res += "Buzz";
+        if(res === '') res = `${i}`;
         answerArr.push(res)
 
     }
