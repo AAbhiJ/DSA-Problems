cdb233239061369dc0d7f1206f5494ee45be96fa
AAbhiJ <abhiasawale510@gmail>
2023-11-08 00:45:23 +0530

    added 561. Array Partition

---
+++ leetcode/Arrays/561. Array Partition/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * TC : O(NlogN)
+ * SC : O(1)
+ */
+var arrayPairSum = function(nums) {
+    nums = nums.sort((a,b)=>a-b);
+    let sum = 0;
+    for(let i = 0; i < nums.length; i+=2){
+        sum += Math.min(nums[i], nums[i+1]);
+    }
+    return sum;
+};
